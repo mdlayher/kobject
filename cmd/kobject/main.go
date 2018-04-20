4162706302faff9644540d46d78fc127e1f0385a
Matt Layher <mdlayher@gmail.com>
2018-04-20 12:14:04 -0400

    cmd/kobject: initial commit

---
+++ cmd/kobject/main.go
@@ -0,0 +1,26 @@
+// Command kobject opens a Linux kobject userspace events listener and prints
+// all received events to the terminal.
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/mdlayher/kobject"
+)
+
+func main() {
+	c, err := kobject.New()
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	for {
+		event, err := c.Receive()
+		if err != nil {
+			log.Fatalf("failed to receive: %v", err)
+		}
+
+		fmt.Println(event)
+	}
+}
