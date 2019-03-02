000771b5d6e86a4cfe0d7de26c64700a3c14c473
Matt Layher <mdlayher@gmail.com>
2019-03-02 11:42:40 -0500

    cmd/kobject: optionally set deadlines between receive calls

--- cmd/kobject/main.go
+++ cmd/kobject/main.go
@@ -3,13 +3,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/mdlayher/kobject"
 )
 
 func main() {
+	var (
+		tFlag = flag.Duration("t", 0*time.Second, "the amount of time to wait between events before timing out (default: forever)")
+	)
+	flag.Parse()
+
 	c, err := kobject.New()
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -17,6 +24,12 @@ func main() {
 	defer c.Close()
 
 	for {
+		if *tFlag > 0 {
+			if err := c.SetDeadline(time.Now().Add(*tFlag)); err != nil {
+				log.Fatalf("failed to set deadline: %v", err)
+			}
+		}
+
 		event, err := c.Receive()
 		if err != nil {
 			log.Fatalf("failed to receive: %v", err)
